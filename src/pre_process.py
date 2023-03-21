8cc332d992f390fdea9d47e9fc69fcafdfe7314b
ssm498 <s.fernando@ufl.com>
2023-03-21 14:31:33 -0500

    Added 3 new functions for pre-processing

--- src/pre_process.py
+++ src/pre_process.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 # Import dataset (should be 35 attributes)
 def getTrain():
        return getFile("data/raw/train.csv")
@@ -32,7 +32,7 @@ def getFile(csv):
                         'Verification Status': str,
                         'Payment Plan': str,
                         'Loan Title': str,
-                        'Debt to Income': float,
+                        'Debit to Income': float,
                         'Delinquency - two years': int,
                         'Inquiries - six months': int,
                         'Open Accounts': int,
@@ -56,3 +56,39 @@ def getFile(csv):
                        },
                      na_values=['-'], 
                      encoding = "ISO-8859-1")
+
+def findMissingValues(df):
+    """
+    Finds any cells with 'NA' in it. Might need to expand to other 'missing' 
+    data indicators.
+    """
+    missing_values = []
+    for row in range(df.shape[0]):
+        for col in range(df.shape[1]):
+            # Not sure if there are any other 'na' indicators like '-' in the dataset
+            if pd.isna(df.iloc[row, col]):
+                missing_values.append((row, col))
+    return missing_values
+
+def detectOutliers(df, col_name, z_thresh=3):
+    """
+    Takes in a data frame, column name, and z-score threshold and 
+    returns a list of indexes for rows containing outliers in each given column
+    """
+    col_mean = np.mean(df[col_name])
+    col_std = np.std(df[col_name])
+    z_scores = abs((df[col_name] - col_mean) / col_std)
+    return list(np.where(z_scores > z_thresh)[0])
+
+def getZScores(df, colTypes):
+    """
+    Takes in a data frame and a dictionary of column names with their types and
+    returns a dictionary of column names as keys and a list of outliers as values
+    """
+    colWithZScore = {}
+    for key, value in colTypes.items():
+       if value in [int, float]:
+           colWithZScore.setdefault(key, [])
+           colWithZScore[key].append(detectOutliers(df, key))
+    return colWithZScore
+           
